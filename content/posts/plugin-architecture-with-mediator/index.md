5f6945f86b00759f2668f844a72b0263ce0c6f96
Walker Hannan <awahannan@gmail.com>
2024-07-20 21:54:38 -0400

    Allow diagrams build script to output to posts directory directly

---
+++ content/posts/plugin-architecture-with-mediator/index.md
@@ -0,0 +1,23 @@
+---
+title: "Plugin Architecture with the Mediator Pattern"
+date: 2024-07-18T10:55:59-04:00
+draft: true
+tags:
+    - software-architecture
+    - design-patterns
+    - blazor
+    - aspnet-core
+    - dotnet
+---
+
+# Background and requirements
+
+# Design goals
+
+# How it works
+
+![menu sequence diagram](images/menu.sequence.png)
+
+![query sequence diagram](images/query.sequence.png)
+
+![results sequence diagram](images/results.sequence.png)
--- diagrams/build.js
+++ diagrams/build.js
@@ -2,21 +2,20 @@ const plantuml = require('node-plantuml');
 const fs = require('fs');
 
 async function build(path) {
-  const dir = await fs.promises.opendir(path)
+  const dir = await fs.promises.opendir(path, {recursive: true})
   for await (const file of dir) {
     if (!file.name.endsWith('.plantuml')) {
       continue;
     }
 
-    await generate(file.name, file.name.replace('.plantuml', '.png'), 'png');
+    await generate(file.parentPath, file.name, file.name.replace('.plantuml', '.png'), 'png');
   }
 }
 
-async function generate(inputFileName, outputFileName, outputFileType) {
-  const genSvg = plantuml.generate(inputFileName, { format: outputFileType });
+async function generate(inputFilePath, inputFileName, outputFileName, outputFileType) {
+  const genSvg = plantuml.generate(`${inputFilePath}/${inputFileName}`, { format: outputFileType });
 
-  const outDir = `./images/${outputFileType}`
-  await createDirIfNotExists('./images');
+  const outDir = `../content/${inputFilePath}/images`
   await createDirIfNotExists(outDir);
   genSvg.out.pipe(fs.createWriteStream(`${outDir}/${outputFileName}`));
 }
